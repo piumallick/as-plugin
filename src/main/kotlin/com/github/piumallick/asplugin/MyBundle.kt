97daddf42a48f8c83b98edd5763597322d227c69
GitHub Action <action@github.com>
2024-01-25 22:56:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/piumallick/asplugin/MyBundle.kt
+++ src/main/kotlin/com/github/piumallick/asplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.piumallick.asplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/piumallick/asplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/piumallick/asplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.piumallick.asplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
